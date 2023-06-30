b6a3655b79176950107fb5975c4406ffec066528
Sinan Tunç <sinantunc878@gmail.com>
2023-06-30 09:36:31 +0300

    Added data retrieval and question display functions

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const startGame = () => {
-    getQuestions();
-};
+"use strict";
+
+// define variables
+
+let remember = 0;
+const userAnswers = [];
+// end
+
+const gameSection = document.querySelector(".game-section");
+
+// start game
 
 const getQuestions = async () => {
   try {
-    const response = await fetch("file1.json");
-    const data = await response.json();
+    const response = await fetch("questions.json"); // from local questions.json file
+    const data = await response.json(); // data ready
 
     displayQuestions(data);
   } catch (error) {
@@ -13,13 +21,63 @@ const getQuestions = async () => {
   }
 };
 
+function startGame() {
+  // hidden start screen
+
+  document.querySelector(".start-section").classList.add("d-none");
+
+  //e.target.classList.add("d-none");
+
+  // show game screen
+
+  gameSection.classList.remove("d-none");
+
+  // start get data
+
+  getQuestions();
+}
+
 const displayQuestions = (data) => {
+  if (data.length) {
+    /*
+    [
+       0:{
+        "id": 1,
+        "question": "2 + 2 = ?",
+        "answers": {
+            "a": "1",
+            "b": "2",
+            "c": "4",
+            "d": "5"
+        },
+        "correct_answer": "c"
+    },
+    ]
+   */
+
+    const question = document.querySelector("#question");
+
+    const answers = document.getElementsByTagName("label");
+
+
+    question.textContent = remember+1 + ")  " + data[remember]["question"];
+
+    const ans = data[remember]["answers"];
+
+
+
+    Object.entries(ans).forEach((value, index, array)=>{
+      answers[index].textContent =value[0].toUpperCase() + ") " + value[1];
+    })
+
     
-} 
-    
 
-  
+  } else {
+    gameSection.textContent = "Not found data!";
+  }
+};
 
-
-
-//$response = Invoke-WebRequest -Uri "https://quizapi.io/api/v1/questions?apiKey=tEuVDyZu6iKOH9zOuZy35nkEaRRqvZopYYO4aIjc&limit=50&difficulty=easy" -Method "GET" -OutFile "file1.json"
+function handleSubmit(e){
+  e.preventDefault();
+  console.log(e);
+}
